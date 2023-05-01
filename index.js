598f7fa5e01b720b8020a40e541f02dd2cecd4fd
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-01 19:28:50 +0600

    load-types-of-data

--- index.js
+++ index.js
@@ -15,8 +15,35 @@ app.get("/categories", (req, res) => {
 	res.send(categories);
 });
 
+app.get("/categories/:id", (req, res) => {
+	const id = parseInt(req.params.id);
+	if (id === 0) {
+		res.send(news);
+	} else {
+		const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+		res.send(categoryNews);
+	}
+});
+
+// app.get("/categories/:id", (req, res) => {
+// 	const id = parseInt(req.params.id);
+// 	if (id === 0) {
+// 		res.send(news);
+// 	} else {
+// 		const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+// 		res.send(categoryNews);
+// 	}
+// });
+
 app.get("/news", (req, res) => {
 	res.send(news);
+});
+
+// Specific News
+app.get("/news/:id", (req, res) => {
+	const id = req.params.id;
+	const selectedNews = news.find((n) => n._id === id);
+	res.send(selectedNews);
 });
 
 app.listen(port, () => {
