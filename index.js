cbe97f0d72f02afa118355ee6dfcce3c9338a527
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-01 10:55:11 +0600

    middleware-cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const port = process.env.PORT || 5000;
 const categories = require("./Data/categories.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send("The Dragon is coming soon...");
